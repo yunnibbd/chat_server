c66eba3e15d4d1956af522a0ea8556bcd9cd451d
yunni <yunnibbd@163.com>
2021-01-29 11:40:48 +0800

    增加 chat_server 注释

--- chat_server/chat_server.cpp
+++ chat_server/chat_server.cpp
@@ -13,21 +13,37 @@ using namespace boost::asio::ip;
 using chat_message_queue = deque<chat_message>;
 
 class chat_session;
-using chat_session_ptr = shared_ptr<chat_session>;
 
 //room
 class chat_room {
 public:
+	using chat_session_ptr = shared_ptr<chat_session>;
+
+	/**
+	 * @brief 客户端加入事件
+	 * @param cp 客户端智能指针
+	 * @return
+	 */
 	void join(chat_session_ptr cp) {
 		chat_sessions_.insert(cp);
 		for (const auto &msg : recent_msgs_)
 			cp->deliver(msg);
 	}
 
+	/**
+	 * @brief 客户端离开事件
+	 * @param cp 客户端智能指针
+	 * @return
+	 */
 	void leave(chat_session_ptr cp) {
 		chat_sessions_.erase(cp);
 	}
 
+	/**
+	 * @brief 给所有客户端分发消息
+	 * @param msg 消息引用
+	 * @return
+	 */
 	void deliver(const chat_message &msg) {
 		recent_msgs_.push_back(msg);
 		while (recent_msgs_.size() > max_recent_msgs)
@@ -52,11 +68,21 @@ public:
 
 	}
 
+	/**
+	 * @brief 触发客户端加入事件并读取消息头
+	 * @param
+	 * @return
+	 */
 	void start() {
 		room_.join(shared_from_this());
 		do_read_header();
 	}
 
+	/**
+	 * @brief 将消息发送到服务端
+	 * @param msg 消息引用
+	 * @return
+	 */
 	void deliver(const chat_message &msg) {
 		bool write_in_progress = !write_msgs_.empty();
 		write_msgs_.push_back(msg);
@@ -67,6 +93,11 @@ public:
 	}
 
 private:
+	/**
+	 * @brief 读取消息头
+	 * @param
+	 * @return
+	 */
 	void do_read_header() {
 		auto self(shared_from_this());
 		boost::asio::async_read(
@@ -83,6 +114,11 @@ private:
 		);
 	}
 
+	/**
+	 * @brief 读取消息体
+	 * @param
+	 * @return
+	 */
 	void do_read_body() {
 		auto self(shared_from_this());
 		boost::asio::async_read(
@@ -102,6 +138,11 @@ private:
 	}
 
 	
+	/**
+	 * @brief 重复发送消息队列中第一条,直至发送完成
+	 * @param
+	 * @return
+	 */
 	void do_write() {
 		auto self(shared_from_this());
 		boost::asio::async_write(
@@ -131,6 +172,13 @@ private:
 //chat server
 class chat_server {
 public:
+	/**
+	 * @brief 构造函数,并投递一个接受客户端的任务
+	 * @param io_service
+	 * @param endpoint 服务端协议和端口
+	 * @param server_id 测试用,本服务的id
+	 * @return 返回当前类对象
+	 */
 	chat_server(boost::asio::io_service &io_service,
 		const tcp::endpoint &endpoint, int server_id = -1) 
 		: acceptor_(io_service, endpoint), socket_(io_service), server_id_(server_id) {
@@ -139,6 +187,11 @@ public:
 	}
 
 private:
+	/**
+	 * @brief 接受新客户端
+	 * @param
+	 * @return
+	 */
 	void do_accept() {
 		acceptor_.async_accept(socket_, [this](boost::system::error_code ec) {
 			if (!ec) {
